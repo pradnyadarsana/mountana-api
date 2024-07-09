29103d47a40ba97fbe51549ee8ea6175a02086a4
pradnyadarsana <pradnyadarsana13@gmail.com>
2024-07-09 22:55:59 +0700

    init commit

---
+++ src/main/java/com/mountana/mountana_api/MountanaApiApplication.java
@@ -0,0 +1,13 @@
+package com.mountana.mountana_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MountanaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MountanaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mountana/mountana_api/MountanaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mountana.mountana_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MountanaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
